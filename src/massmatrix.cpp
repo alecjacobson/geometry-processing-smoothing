6a677aae0961eba02ba1724f4a5e9287d7f3ba35
rkevingibson <rkevingibson@gmail.com>
2017-02-07 13:53:16 -0500

    Finished, seems to work correctly

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -6,5 +6,27 @@ void massmatrix(
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
   // Add your code here
+
+	int num_verts = F.maxCoeff() + 1;
+	Eigen::VectorXd masses(num_verts);
+	masses.setZero();
+
+	for (int i = 0; i < F.rows(); i++) {
+		//Compute the area of this triangle using heron's formula.
+		double a = l(i, 0);
+		double b = l(i, 1);
+		double c = l(i, 2);
+		double s = (a + b + c) / 2;
+		double A = std::sqrt(s*(s - a)*(s - b)*(s - c))/3.0;
+
+		for (int j = 0; j < 3; j++) {
+			masses(F(i, j)) += A;
+		}
+	}
+
+	
+
+	M = masses.asDiagonal();
+	
 }
 
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,12 +1,38 @@
 #include "smooth.h"
 
+#include "massmatrix.h"
+#include "cotmatrix.h"
+#include "igl/edge_lengths.h"
+
 void smooth(
-    const Eigen::MatrixXd & V,
-    const Eigen::MatrixXi & F,
-    const Eigen::MatrixXd & G,
-    double lambda,
-    Eigen::MatrixXd & U)
+	const Eigen::MatrixXd & V,
+	const Eigen::MatrixXi & F,
+	const Eigen::MatrixXd & G,
+	double lambda,
+	Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+	Eigen::MatrixXd l;
+	igl::edge_lengths(V, F, l);
+
+	Eigen::SparseMatrix<double> L;
+	Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+	cotmatrix(l, F, L);
+	massmatrix(l, F, M);
+	
+	Eigen::SparseMatrix<double> mass;
+	{
+		std::vector<Eigen::Triplet<double>> entries;
+		entries.reserve(M.diagonal().size());
+		for (int i = 0; i < M.diagonal().size(); i++) {
+			entries.emplace_back(i, i, M.diagonal()(i));
+		}
+		mass.resize(entries.size(), entries.size());
+		mass.setFromTriplets(entries.begin(), entries.end());
+	}
+
+	Eigen::SparseMatrix<double> A = (mass - lambda*L);
+	Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> cholesky(A);
+	Eigen::MatrixXd b = (M*G).eval();
+
+	U = cholesky.solve(b);
 }
