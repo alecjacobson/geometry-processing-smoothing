3eda47b9036af3c9dd5eaa5081bba2ba515a8dc3
leng <maryelaine.malit@gmail.com>
2017-02-08 11:16:55 -0500

    working?

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,10 +1,28 @@
 #include "massmatrix.h"
+#include <math.h>
+#include <iostream>
+
+// Return the area of the triangle with sides a, b and c
+double get_area(double a, double b, double c) {
+  double s = 0.5 * (a + b + c);
+  return sqrt(s * (s - a) * (s - b) * (s - c));
+}
 
 void massmatrix(
   const Eigen::MatrixXd & l,
   const Eigen::MatrixXi & F,
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
-  // Add your code here
+  M.setZero();
+  int v0, v1, v2;
+  double area;
+  for (int i = 0; i < F.rows(); i++) {
+    v0 = F(i,0);
+    v1 = F(i,1);
+    v2 = F(i,2);
+    area = get_area(l(i,0), l(i,1), l(i,2)) / (double) 3;
+    M.diagonal()[v0] += area;
+    M.diagonal()[v1] += area;
+    M.diagonal()[v2] += area;
+  }
 }
-
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,9 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
+#include <igl/edge_lengths.h>
+#include <algorithm>
+#include <iostream>
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -7,6 +12,29 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+  // construct l - the length of the edges
+  Eigen::MatrixXd l(F.rows(), 3);
+  igl::edge_lengths(V, F, l);
+  
+  int num_vert = V.rows();
+  Eigen::SparseMatrix<double> L(num_vert, num_vert);
+  cotmatrix(l, F, L);
+  
+  Eigen::DiagonalMatrix<double,Eigen::Dynamic> M(num_vert);
+  massmatrix(l, F, M);
+  
+  // Can't perform arithmetic b/w Diag & Sparse, so sparsify M
+  Eigen::SparseMatrix<double> sparseM(num_vert, num_vert);
+  sparseM.reserve(num_vert);
+  sparseM.setZero();
+  std::vector<Eigen::Triplet<double> > tripletList;
+  for (int i = 0; i < num_vert; i++) {
+    tripletList.push_back(Eigen::Triplet<double>(i, i, M.diagonal()[i]));
+  }
+  sparseM.setFromTriplets(tripletList.begin(), tripletList.end());
+  
+  L = lambda * L;
+  Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper> cg;
+  cg.compute(sparseM - L);
+  U = cg.solve(sparseM * G);
 }
