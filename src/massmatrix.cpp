5c9b81b57f2671c1d2db95212e9d1177f3a8ba46
M-Anwar <muhammedshabbeer@hotmail.com>
2017-02-09 03:35:48 -0500

    Completed Working
    - Working on the provided examples

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,10 +1,25 @@
 #include "massmatrix.h"
 
+#include <igl/doublearea.h>
 void massmatrix(
   const Eigen::MatrixXd & l,
   const Eigen::MatrixXi & F,
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
-  // Add your code here
+	// Add your code here
+	Eigen::VectorXd mass(F.maxCoeff() + 1);
+	mass.setZero();
+	Eigen::VectorXd area(F.rows());
+
+	igl::doublearea(l, area); // Area of triangle faces in order
+
+	for (int i = 0; i < F.rows(); i++) {
+		for (int j = 0; j < 3; j++) {
+			mass(F(i, j)) += area(i) /3;
+		}
+	}
+
+	M = mass.asDiagonal();
+
 }
 
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,5 +1,7 @@
 #include "smooth.h"
-
+#include "massmatrix.h"
+#include "cotmatrix.h"
+#include <igl/edge_lengths.h>
 void smooth(
     const Eigen::MatrixXd & V,
     const Eigen::MatrixXi & F,
@@ -7,6 +9,31 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+	// Replace with your code
+	
+	//Calculate edge-lengths
+	Eigen::MatrixXd l;
+	igl::edge_lengths(V, F, l);
+
+	//Get the cotangent and mass information
+	Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+	massmatrix(l, F,  M);
+	Eigen::SparseMatrix<double> L;
+	cotmatrix(l, F, L);
+
+	//Convert the mass diagonal matrix, into a sparse matrix
+	Eigen::SparseMatrix<double> m;	
+	std::vector<Eigen::Triplet<double>> triplets;
+	triplets.reserve(M.diagonal().size());
+	m.resize(V.rows(), V.rows());
+	m.setZero();
+	for (int i = 0; i < V.rows(); i++) {
+		triplets.emplace_back(i, i, M.diagonal()[i]);
+	}
+	m.setFromTriplets(triplets.begin(), triplets.end());
+
+	//Solve system using cholesky-decomposition
+	Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> cholesky_decomp;
+	cholesky_decomp.compute(m - lambda*L);
+	U = cholesky_decomp.solve(m * G);
 }
