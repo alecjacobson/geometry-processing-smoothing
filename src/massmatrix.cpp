0196ad2a9f9961f7f9c3e9b09e60e92a79cef4f9
AdamSturge <AdamSturge991@hotmail.com>
2017-02-10 18:15:10 -0500

    done I think

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -5,6 +5,21 @@ void massmatrix(
   const Eigen::MatrixXi & F,
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
-  // Add your code here
+	int m = F.rows();
+	for (int t = 0; t < m; ++t) {
+		Eigen::RowVectorXi triangle = F.row(t);
+		int i = triangle(0);
+		int j = triangle(1);
+		int k = triangle(2);
+		double a = l(t, 0);
+		double b = l(t, 1);
+		double c = l(t, 2);
+		double s = 0.5*(a + b + c);
+		double area = sqrt(s*(s - a)*(s - b)*(s - c))/3;
+		M.diagonal()(i) += area;
+		M.diagonal()(j) += area;
+		M.diagonal()(k) += area;
+	}
+	
 }
 
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,19 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
+#include "igl\edge_lengths.h"
+
+
+void sparseify(const Eigen::DiagonalMatrix<double, Eigen::Dynamic> & M, Eigen::SparseMatrix<double> & S) {
+	int m = M.diagonal().size();
+	std::vector<Eigen::Triplet<double>> tripletList;
+	tripletList.reserve(m);
+	Eigen::VectorXd diag = M.diagonal();
+	for (int i = 0; i < m; ++i) {
+		tripletList.push_back(Eigen::Triplet<double>(i, i, diag(i)));
+	}
+	S.setFromTriplets(tripletList.begin(), tripletList.end());
+}
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -7,6 +22,33 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+	int m = F.rows();
+
+	int nV = F.maxCoeff() + 1;
+	Eigen::SparseMatrix<double> L(nV,nV);
+	L.setZero();
+	Eigen::DiagonalMatrix<double, Eigen::Dynamic> M(nV);
+	M.setZero();
+
+	Eigen::SparseMatrix<double> SM(nV, nV);
+	Eigen::SparseMatrix<double> A;
+	Eigen::MatrixXd AL;
+	Eigen::MatrixXd l(m, 3);
+
+	igl::edge_lengths(V, F, l);
+	cotmatrix(l, F, L);
+	massmatrix(l, F, M);
+	sparseify(M, SM);
+	A = SM - lambda*L;
+	//Eigen::LLT<Eigen::MatrixXd> lltOfA(A);
+	//AL = lltOfA.matrixL();
+		
+	Eigen::MatrixXd b = M*G;
+	Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> chol(A);
+	U = chol.solve(b);
+
+	std::cout << "DONE" << std::endl;
+  
+
 }
+
