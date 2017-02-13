c5c4faad55a1a52e59e2275133d9c10ca411c4a9
Alex Tessier <alex.tessier@autodesk.com>
2017-02-12 23:49:22 -0500

    Seems to be working after some bracket mistakes got fixed in my cotangent code. Smoothing the beetle geometry makes it disappear after 4 iterations. Lambda of 0.01 seems to be the limit of how big we'd want lambda to be as it washes out detail. Lambda of 0.0001 seems like a good value.

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,5 +1,22 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
 
+#include <igl/edge_lengths.h>
+
+#include <Eigen/SparseCholesky>
+
+//
+// M^t+1 V^t = (M^t+1 - lambda L^t+1) V^t+1
+// assume that small changes in V have a negligeable effect, discretize explicitly:
+// M^t V^t = (M^t - lambda L^t) V^t+1
+// in our case/notation:
+// M^t G^t = (M^t - lambda L^t) U
+//
+// Solve A x = b
+// A -> (M^t - lambda L^t)
+// x -> U
+// b -> M^t G^t
 void smooth(
     const Eigen::MatrixXd & V,
     const Eigen::MatrixXi & F,
@@ -7,6 +24,42 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+    static bool debug( false );
+    
+    Eigen::MatrixXd l;
+    igl::edge_lengths( V, F, l );
+    
+    // get L and M
+    Eigen::SparseMatrix<double> L;
+    cotmatrix( l, F, L );
+    
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+    massmatrix( l, F, M );
+
+    // and our A is (M^t+lambda L^t)
+    Eigen::SparseMatrix<double> A = -lambda * L;
+    // Have to add M^t to A's diagonal.
+    //A = M.transpose() + A
+    //A.diagonal() += M.diagonal();
+    
+    const int n( M.diagonal().size() );
+    for( int i=0; i<n; ++i )
+    {
+        if( debug )
+            std::cout<<"A("<<i<<","<<i<<"): "<<A.coeffRef(i,i)<<std::endl;
+        A.coeffRef( i, i ) += M.diagonal()(i);
+        if( debug )
+            std::cout<<"new A("<<i<<","<<i<<"): "<<A.coeffRef(i,i)<<std::endl;
+    }
+
+   
+
+    Eigen::MatrixXd b;
+    b = M * G;
+
+    if( debug )
+        std::cout<<"A:"<<A<<std::endl;
+    
+    Eigen::SimplicialLDLT< Eigen::SparseMatrix<double> > cholesky( A );
+    U = cholesky.solve( b );
 }
