3bd813eeca4022e654bdeaabe5e8111428ed6fbd
Timothy Jeruzalski <timjj22@gmail.com>
2017-02-15 01:36:08 -0500

    Algorithms implemented, but does not seem to be working in any case. Solver only returns NaN's

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,8 @@
 #include "smooth.h"
+#include "massmatrix.h"
+#include "cotmatrix.h"
+#include <igl/edge_lengths.h>
+#include <Eigen/SparseCholesky>
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -7,6 +11,47 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+  /*
+    Using the cotmatrix and massmatrix functions, smooth the mesh in V & F
+
+    the mesh update rule is performed:
+
+    Mᵗ⁺¹ Vᵗ = (Mᵗ - λLᵗ) Vᵗ⁺¹
+
+    where the t is the current smoothing "time"
+    We can assume that the changes in V have a negligible effect on L and M, and can discretize explicitly through
+
+    Mᵗ Vᵗ = (Mᵗ - λLᵗ) Vᵗ⁺¹
+
+    We can also generalize this to have an effect via smoothing of a mesh, or via smoothing a function along a mesh.
+    This is done by using G as the function to be smoothed, and can be either vertices of the mesh or function along the surface:
+
+    Mᵗ Gᵗ = (Mᵗ - λLᵗ) Vᵗ⁺¹
+
+    Which can be sparsely solved
+   */
+
+  // get edge lengths
+  Eigen::MatrixXd l(F.rows(), 3);
+  igl::edge_lengths(V, F, l);
+
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+  massmatrix(l, F, M);
+
+  Eigen::SparseMatrix<double> L;
+  cotmatrix(l, F, L);
+
+  Eigen::SparseMatrix<double> A(M.rows(), M.rows());
+  
+  A = -lambda*L;
+
+  for(int32_t i = 0; i < A.rows(); i++)
+  {
+    A.coeffRef(i, i) += M.diagonal()(i);
+  }
+  
+  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver(A);
+  U = solver.solve(M*G);
+  if(U.maxCoeff()  != U.maxCoeff())
+    printf("NaN in solution\n");
 }
