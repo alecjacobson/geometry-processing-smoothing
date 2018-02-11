b11057cfb25c65e1ac65f0b6e27f3727c3e33e07
Michelle <m.arkhangorodsky@gmail.com>
2018-02-11 13:43:05 -0500

    mass mastrix implemented

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -5,6 +5,25 @@ void massmatrix(
   const Eigen::MatrixXi & F,
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
-  // Add your code here
-}
+  int num_faces = F.rows();
+  int V = F.maxCoeff();
+  Eigen::VectorXd masses(V);
+  // TODO: can preprocess to do check of is this vertex in triangle faster
+  for(int i = 0; i < V; i++){
+    // find all triangles t that contain vertex i
+    double mass = 0;
+    for(int f_idx = 0; f_idx < num_faces; f_idx++){
+      if(F(f_idx,0) == i || F(f_idx, 1) == i || F(f_idx, 2) == i){
+        // get edge lengths
+        double a = l(f_idx, 0); double b = l(f_idx, 1); double c = l(f_idx, 0);
+        // Herons formula for area of triangle
+        double sp = (a + b + c) / 2; // semiperimeter
+        double area = sqrt(sp * (sp - a) * (sp - b) * (sp * c));
+        mass += area;
+      }
+    }
+    masses(i) = mass;
+  }
 
+  M = masses.asDiagonal();
+}
