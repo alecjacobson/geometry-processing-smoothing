fa3aa5a3530b49207460196e03d427916c72cf7c
Ziheng Liang <ziheng941008@hotmail.com>
2018-02-12 00:02:02 -0500

    finish massmatrix

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,4 +1,5 @@
 #include "massmatrix.h"
+#include <math.h>
 
 void massmatrix(
   const Eigen::MatrixXd & l,
@@ -6,5 +7,19 @@ void massmatrix(
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
   // Add your code here
+
+  int vertexNum = F.maxCoeff() + 1;
+  Eigen::VectorXd areas = Eigen::VectorXd::Zero(vertexNum);
+  for (int i = 0;i<F.rows();i++) {
+  	double a = l(i, 0);
+  	double b = l(i, 1);
+  	double c = l(i, 2);
+    double s = (a + b + c) / 2;
+    double area = sqrt(s * (s - a) * (s - b) * (s - c)) / 3;
+    areas(F(i, 0)) += area;
+    areas(F(i, 1))  += area;
+    areas(F(i, 2))  += area;
+  }
+  M = areas.asDiagonal();
 }
 
