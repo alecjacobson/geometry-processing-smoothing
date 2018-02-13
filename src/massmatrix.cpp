ae3c833c37b78136c2e16444af78963c43773fd4
sarahanne <sak@dgp.toronto.edu>
2018-02-12 22:38:08 -0500

    done with all the parts but i haven't run it yet

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -9,19 +9,30 @@ void massmatrix(
 
   M.resize(F.maxCoeff() + 1);
 
+  float a, b, c, area, area_sum = 0;
+  Eigen::ArrayXi face(3);
+  Eigen::VectorXf sorted(3);
 
   for (int i = 0; i < F.rows(); i++)
   {
-	  Eigen::ArrayXi face = F.row(i);
-	  if ((face == i).any())
+	  area_sum = 0;
+	  for (int j = 0; j < F.rows(); j++)
 	  {
-		  float a = l(i, 0);
-		  float b = l(i, 1);
-		  float c = l(i, 2);
-		  double s = (a + b + c) / 2; //semiperimeter
-		  double area = sqrt(s*(s - a)*(s - b)*(s - c)); //Heron's formula
+		  face = F.row(j);
+		  if ((face == j).any())
+		  {
+			  sorted << l(i, 0), l(i, 1), l(i, 2);
+			  std::sort(sorted.data(), sorted.data() + sorted.size());
+			  a = sorted[0];
+			  b = sorted[1];
+			  c = sorted[2];
+			  //double s = (a + b + c) / 2; //semiperimeter
+			  //area = sqrt(s*(s - a)*(s - b)*(s - c)); //Heron's formula
+			  area = 0.25 * sqrt((a + (b + c)) * (c - (a - b)) * (c + (a - b)) * (a + (b - c))); // stable Heron's formula
+			  area_sum += area;
+		  }
 	  }
-
+	  M.diagonal()[i] = (1/3) * area_sum;
   }
 
 }
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,8 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
+#include <igl/edge_lengths.h>
+#include <Eigen\src\Cholesky\LLT.h>
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -8,5 +12,27 @@ void smooth(
     Eigen::MatrixXd & U)
 {
   // Replace with your code
-  U = G;
+  //U = G;
+  Eigen::MatrixXd l;
+  igl::edge_lengths(V, F, l);
+  
+  Eigen::SparseMatrix<double> L;
+  cotmatrix(l, F, L);
+
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+  massmatrix(l, F, M);
+
+  Eigen::SparseMatrix<double> A;
+  A = -lambda * L;
+
+  for (int i = 0; i < A.rows(); i++)
+  {
+	  A.coeffRef(i, i) += M.diagonal()(i);
+  }
+  //Eigen::MatrixXd A = M - lambda * L;
+  Eigen::LLT<Eigen::MatrixXd> llt;
+  llt.compute(A);
+  Eigen::VectorXf b = M*G;
+  U = llt.solve(b);
+   
 }
