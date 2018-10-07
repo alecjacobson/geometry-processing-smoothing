eee48d7b16c1cbe710156f714876e2c6f9dfe3bb
Yihuan Mao <yihuan.mao@mail.utoronto.ca>
2018-10-07 00:05:35 -0400

    Yihuan Mao submission

--- src/cotmatrix.cpp
+++ src/cotmatrix.cpp
@@ -1,10 +1,33 @@
 #include "cotmatrix.h"
+#include "math.h"
+
+double cot(double a,double b,double c){
+	double s=(a+b+c)/2;
+	double area=sqrt(s*(s-a)*(s-b)*(s-c));
+	double sin=area*2/b/c;
+	double cos=(b*b+c*c-a*a)/2/b/c;
+	return cos/sin;
+}
 
 void cotmatrix(
   const Eigen::MatrixXd & l,
   const Eigen::MatrixXi & F,
   Eigen::SparseMatrix<double> & L)
 {
-  // Add your code here
+  L.resize(F.maxCoeff()+1,F.maxCoeff()+1);
+  int f=F.rows();
+  double c;
+  typedef Eigen::Triplet<double> T;
+  std::vector<T> list;
+  for (int i=0; i<f; i++){
+  	for (int j=0; j<3; j++){
+  	  c=cot(l(i,j),l(i,(j+1)%3),l(i,(j+2)%3))/2;
+  	  list.push_back(T(F(i,(j+1)%3),F(i,(j+2)%3),c));
+  	  list.push_back(T(F(i,(j+2)%3),F(i,(j+1)%3),c));
+  	  list.push_back(T(F(i,(j+1)%3),F(i,(j+1)%3),-c));
+  	  list.push_back(T(F(i,(j+2)%3),F(i,(j+2)%3),-c));
+	}
+  }
+  L.setFromTriplets(list.begin(), list.end());
 }
 
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,8 @@
 #include "smooth.h"
+#include <Eigen/Cholesky>
+#include <igl/edge_lengths.h>
+#include "massmatrix.h"
+#include "cotmatrix.h"
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -7,6 +11,22 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+  Eigen::MatrixXd l;
+  Eigen::SparseMatrix<double> L,A;
+  Eigen::DiagonalMatrix<double,Eigen::Dynamic> M;
+  igl::edge_lengths(V, F, l);
+  cotmatrix(l,F,L);
+  massmatrix(l,F,M);
+  Eigen::MatrixXd b=M*G;
+  typedef Eigen::Triplet<double> T;
+  std::vector<T> list;
+  Eigen::VectorXd M1=M.diagonal();
+  for (int i=0; i<M1.size(); i++)
+    list.push_back(T(i,i,M1(i)));
+  A.resize(M.rows(),M.rows());
+  A.setFromTriplets(list.begin(), list.end()); 
+  A-=lambda*L;
+  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
+  solver.compute(A);
+  U=solver.solve(b);
 }
