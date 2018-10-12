5e3acf4287638bdc8e05d2b2c956050e5f88d8cd
Simeon Krastnikov <simeon.krastnikov@mail.utoronto.ca>
2018-10-11 22:14:47 -0400

    HW3 Submission

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,10 +1,18 @@
 #include "massmatrix.h"
+#include <math.h>
 
 void massmatrix(
   const Eigen::MatrixXd & l,
   const Eigen::MatrixXi & F,
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
-  // Add your code here
+  M.resize(F.maxCoeff() + 1);
+  for (int f = 0; f < F.rows(); f++) {
+    double s = l.row(f).sum() / 2;
+    double f_A = sqrt(s * (s-l(f,0)) * (s-l(f,1)) * (s-l(f,2)));
+    for (int v = 0; v < 3; v++) {
+      int i = F(f, v);
+      M.diagonal()[i] += f_A / 3;
+    }
+  }
 }
-
