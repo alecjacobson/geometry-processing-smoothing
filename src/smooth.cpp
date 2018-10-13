7559747e15967ea2e51570a474b4419a0a0066a8
eriszhang <jiayieris.zhang@mail.utoronto.ca>
2018-10-13 17:18:51 -0400

    it works

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,10 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
+#include <Eigen/SparseCholesky>
+#include <igl/edge_lengths.h>
+
+using namespace Eigen;
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -7,6 +13,21 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+  // construct cotmatrix and massMatrix
+  MatrixXd l;
+  igl::edge_lengths(V, F, l);
+  SparseMatrix<double> L;
+  DiagonalMatrix<double, Dynamic> M;
+  cotmatrix(l, F, L);
+  massmatrix(l, F, M);
+
+  // solve linear equation: M * G = (M - lam * L) * U
+  SparseMatrix<double> right(M.rows(), M.rows());
+  for(int i = 0; i < M.rows(); i++) {
+  	right.coeffRef(i, i) += M.diagonal()[i];
+  }
+  right -= lambda * L;
+  SimplicialLDLT<SparseMatrix<double>> solver(right);
+  MatrixXd left = M * G;
+  U = solver.solve(left);
 }
