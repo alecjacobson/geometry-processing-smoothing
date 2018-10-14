1cbd20d92caa678bd11637265846eddafa952053
AdrianShe <adrian.she@alumni.ubc.ca>
2018-10-14 18:50:16 -0400

    done

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,10 @@
 #include "smooth.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
+#include "igl/edge_lengths.h"
+#include "igl/cotmatrix.h"
+#include "Eigen/SparseCholesky"
+#include <iostream>
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -8,5 +14,26 @@ void smooth(
     Eigen::MatrixXd & U)
 {
   // Replace with your code
-  U = G;
+  
+  // Compute edge lengths
+  Eigen::MatrixXd l;
+  igl::edge_lengths(V, F, l);
+
+  // Construct cot and mass matrices
+  Eigen::SparseMatrix<double> L_real;
+  Eigen::SparseMatrix<double> L_comp;
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+  cotmatrix(l, F, L_comp); 
+  massmatrix(l, F, M);
+
+  // Solve for U as solution of M G = (M - lambda * L) U;
+  Eigen::SparseMatrix<double> I(F.maxCoeff() + 1, F.maxCoeff() + 1);
+  I.setIdentity();
+  Eigen::SparseMatrix<double> L_lambda = I - lambda * M.inverse() * L_comp;
+  L_lambda = M * L_lambda;
+  Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>> solver;
+  solver.compute(L_lambda);
+  for (int i = 0; i < G.cols(); i++) {
+	U.col(i) = solver.solve(M * G.col(i));
+  }
 }
