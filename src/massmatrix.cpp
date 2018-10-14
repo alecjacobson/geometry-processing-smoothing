43682516f569e9c2ef9d9efd140044dca12dd5be
jnyao <43320994+jnyao@users.noreply.github.com>
2018-10-14 16:46:04 -0400

    Update massmatrix.cpp

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,4 +1,6 @@
 #include "massmatrix.h"
+#include <iostream>
+#include <igl/doublearea.h>
 
 void massmatrix(
   const Eigen::MatrixXd & l,
@@ -6,5 +8,16 @@ void massmatrix(
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
   // Add your code here
-}
+	int nv = F.maxCoeff() + 1;
+	M.resize(F.maxCoeff() + 1);
+	M.setZero();
+	Eigen::MatrixXd dblA(F.rows(), 1);
+	igl::doublearea(l, dblA);
 
+	// Fill in the diagonal entries of M
+	for (int f = 0; f < F.rows(); f++) {
+		M.diagonal()[F(f, 0)] += (1.0 / 6.0) * dblA(f);
+		M.diagonal()[F(f, 1)] += (1.0 / 6.0) * dblA(f);
+		M.diagonal()[F(f, 2)] += (1.0 / 6.0) * dblA(f);
+	}
+}
