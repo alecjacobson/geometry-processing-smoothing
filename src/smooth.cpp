111445e6ea633ac003f94b7926ba6f756665708c
Junrui (John) Xu <johnxu@cs.toronto.edu>
2018-10-16 04:43:55 -0400

    Finish HW3

--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,7 @@
 #include "smooth.h"
+#include "massmatrix.h"
+#include "cotmatrix.h"
+#include <igl/edge_lengths.h>
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -7,6 +10,21 @@ void smooth(
     double lambda,
     Eigen::MatrixXd & U)
 {
-  // Replace with your code
-  U = G;
+    Eigen::MatrixXd l;
+    igl::edge_lengths(V, F, l);
+
+    Eigen::SparseMatrix<double> L;
+    cotmatrix(l, F, L);
+
+    Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+    massmatrix(l, F, M);
+
+    Eigen::SparseMatrix<double> A = -lambda * L;
+    for (int i = 0; i < M.rows(); ++i) {
+        A.coeffRef(i, i) += M.diagonal()(i);
+    }
+
+    Eigen::MatrixXd B = M * G;
+    Eigen::BiCGSTAB<Eigen::SparseMatrix<double>> solver(A);
+    U = solver.solve(B);
 }
